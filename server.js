673b0336ef2d34f186976e803f4ad62a02941708
Naveenak27 <naveenkarnan5@gmail.com>
2025-02-18 23:19:30 +0530

    Update CORS configuration

--- server.js
+++ server.js
@@ -117,7 +117,17 @@ const transporter = nodemailer.createTransport({
     rejectUnauthorized: false
   }
 });
+// Updated CORS configuration
+app.use(cors());
+app.use(express.json());
 
+// Add these headers to all responses
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 // Routes
 app.get('/api/templates', async (req, res) => {
   try {
